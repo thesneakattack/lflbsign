7b6023567184fc4a31681ed7993ff49919328b1e
David F <davidfdz@gmail.com>
2023-02-09 22:42:35 +0000

    preview switched to css transform instead of browser zoom

--- resources/views/preview.blade.php
+++ resources/views/preview.blade.php
@@ -6,9 +6,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>LFLB Sign Preview</title>
+    <script>
+        window.addEventListener("load", () => {
+            console.log('loaded');
+        document.body.style.transform = 'scale(0.5)';
+        document.body.style.transformOrigin = 'top center';
+});
+    </script>
 </head>
 
-<body style="background-color:#333;">
+<body style="background-color:#333;max-height:960px;margin:0;">
     <div>
         <iframe src="/{{$requested_url}}" frameborder="2" width="1080" height="1920"
             style="display:block; width:1080px; margin:0px auto;"></iframe>
