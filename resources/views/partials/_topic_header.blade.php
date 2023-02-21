58e6ec4dd6b2f3951f47bcdc2fa1650f9d22f513
David F <davidfdz@gmail.com>
2023-02-21 00:20:41 +0000

    topic page: grid to flexbox, increase height of banner area, decrease text scaling of body area

--- resources/views/partials/_topic_header.blade.php
+++ resources/views/partials/_topic_header.blade.php
@@ -1,33 +1,28 @@
         <!-- Hero -->
-        <section
-            class="relative h-96 flex flex-col justify-center align-center text-center space-y-4 mb-4"
-        >
-@unless(empty($topic['mainImage']))
-<div
-class="absolute top-0 left-0 w-full h-full opacity-80 bg-no-repeat bg-center bg-tintPrimary bg-blend-multiply"
-style="background-image: url('{{ asset("/assets/".$topic['mainImage']) }}')"
-></div>
-    {{-- <img src = "{{ asset("/assets/".$topic['mainImage']) }}" width="800" /> --}}
-@else
-<div
-class="absolute top-0 left-0 w-full h-full opacity-80 bg-no-repeat bg-center bg-tintPrimary bg-blend-multiply"
-style="background-image: url('{{ asset("/assets/".$topic['coverPhoto']) }}')"
-></div>
-    {{-- <img src = "{{ asset("/assets/".$topic['coverPhoto']) }}" width="800" /> --}}
-@endunless        
+        <section class="relative flex flex-col justify-center mb-4 space-y-4 text-center h-[506px] align-center">
+            @unless(empty($topic['mainImage']))
+                <div class="absolute top-0 left-0 w-full h-full bg-center bg-no-repeat opacity-80 bg-tintPrimary bg-blend-multiply"
+                    style="background-image: url('{{ asset('/assets/' . $topic['mainImage']) }}');background-size:cover;">
+                </div>
+                {{-- <img src = "{{ asset("/assets/".$topic['mainImage']) }}" width="800" /> --}}
+            @else
+                <div class="absolute top-0 left-0 w-full h-full bg-center bg-no-repeat opacity-80 bg-tintPrimary bg-blend-multiply"
+                    style="background-image: url('{{ asset('/assets/' . $topic['coverPhoto']) }}')"></div>
+                {{-- <img src = "{{ asset("/assets/".$topic['coverPhoto']) }}" width="800" /> --}}
+            @endunless
 
 
             <div class="z-10">
                 <h1 class="text-6xl font-bold uppercase text-white drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,1)] mb-8">
-                    {{$topic['title']}}
+                    {{ $topic['title'] }}
                 </h1>
                 <p class="text-3xl text-white font-semibold drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,1)] mt-18 mx-4">
-                    {{strip_tags($topic['introText'])}}
+                    {{ strip_tags($topic['introText']) }}
                 </p>
                 {{-- <div>
                     <a
                         href="register.html"
-                        class="inline-block border-2 border-white text-white py-2 px-4 rounded-xl uppercase mt-2 hover:text-black hover:border-black"
+                        class="inline-block px-4 py-2 mt-2 text-white uppercase border-2 border-white rounded-xl hover:text-black hover:border-black"
                         >Sign Up to List a Gig</a
                     >
                 </div> --}}
