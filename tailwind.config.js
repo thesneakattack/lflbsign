5570908c3a7bdb0ca1ddacce140c33a5e6fa985c
David F <davidfdz@gmail.com>
2023-05-03 05:21:58 +0000

    added custom media query definitions to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,24 @@
 module.exports = {
-    content: [
-        "./resources/**/*.blade.php",
-        "./resources/**/*.js",
-      ],
+    content: ["./resources/**/*.blade.php", "./resources/**/*.js"],
     theme: {
-      extend: {
-        colors: {
-            laravel: "#1f2d61",
-            accentPrimary: "#d3e187",
-            buttonPrimary: "#f36f4f",
-            buttonSecondary: "#09b4ad",
-            buttonTertiary: "#78be20",
-            tintPrimary: "#def2f2",
-          },
-      },
+        extend: {
+            colors: {
+                laravel: "#1f2d61",
+                accentPrimary: "#d3e187",
+                buttonPrimary: "#f36f4f",
+                buttonSecondary: "#09b4ad",
+                buttonTertiary: "#78be20",
+                tintPrimary: "#def2f2",
+            },
+            screens: {
+                "lfd-1080v": {
+                    raw: "((max-width: 1080px) and (max-height: 1920px))",
+                },
+                "lfd-1920h": {
+                    raw: "((max-width: 1080px) and (max-height: 1920px))",
+                },
+            },
+        },
     },
-    plugins: [
-        require('@tailwindcss/typography'),
-    ],
-  }
+    plugins: [require("@tailwindcss/typography")],
+};
